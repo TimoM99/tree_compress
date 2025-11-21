ea09e3ba0fdfa3824c0d887a813e4c99acb43ec9
Timo Martens <timo@himec03.cs.kuleuven.be>
2025-11-21 18:27:41 +0100

    add experiment for pareto front

--- experiment/oc_space_experiment.py
+++ experiment/oc_space_experiment.py
@@ -3,6 +3,7 @@ import click
 import os
 os.environ['PRADA_DATA_DIR']='/cw/dtaijupiter/NoCsBack/dtai/timo/prada_data'
 
+from matplotlib.pylab import pareto
 import veritas
 import tree_compress
 import numpy as np
@@ -15,6 +16,7 @@ import model_params
 
 from sklearn.metrics import balanced_accuracy_score
 from verification import run_verification_tasks, count_ocs
+import pickle
 
 @click.group()
 def cli():
@@ -185,7 +187,91 @@ def verify_compressed_models_cmd(dname, seed, silent, timeout, fold):
             print(json.dumps(results))
 
 
+@cli.command('verify_pareto_models')
+@click.argument("dname")
+@click.option("--seed", default=util.SEED)
+@click.option("--silent", is_flag=True, default=True)
+@click.option("--timeout", default=86400)
+@click.option("--fold", default=0)
+@click.option("--fronts_file", default='pareto_fronts_LOP.pkl')
+def verify_pareto_models_cmd(dname, seed, silent, timeout, fold, fronts_file):
+    np.random.seed(seed)
+    random.seed(seed)
+    pareto_fronts_file = fronts_file
 
+    d, dtrain, dvalid, dtest = util.get_dataset(dname, seed, fold, silent)
+
+    # Load in results that we already have from linear scan
+    results_linear_scan = {}
+    with open('results/linear_scan_after_LOP.txt', 'r') as file: #Load in the linear scan results
+        for line in file:
+            if not line.startswith('{'):
+                continue
+            line_dict = json.loads(line.strip())
+            key = f"{line_dict['dname']}_{line_dict['params']['n_estimators']}_{line_dict['params']['max_depth']}_{line_dict['params']['learning_rate']}_{line_dict['fold']}"
+            results_linear_scan[key] = line_dict
+
+    # Load in the pareto fronts
+    with open(pareto_fronts_file, 'rb') as f:
+        pareto_fronts = pickle.load(f)
+        pareto_fronts = pareto_fronts[dname]
+
+    # print(pareto_fronts)
+    # Now go over all saved xgb models and verify those that are in the pareto front.
+    # Check whether we haven't already verified them in the linear scan results. If so, skip them.
+    # If we have verified them but failed, re-verify them and remove their current entry from linear_scan_after_LOP.txt
+    file_xgb = f"results/xgb_classification_saved.txt"
+    with open(file_xgb, "r") as f:
+        for line in f:
+            if not line.startswith('{'):
+                continue
+            line_dict = json.loads(line.strip())
+            if line_dict['dname'] != dname or line_dict['fold'] != fold:
+                continue
+            # Check if this model is in the pareto front, if not we don't bother verifying it.
+            if pareto_fronts[(pareto_fronts['n_estimators'].astype(float) == float(line_dict['params']['n_estimators'])) &
+                        (pareto_fronts['max_depth'].astype(float) == float(line_dict['params']['max_depth'])) &
+                        (pareto_fronts['learning_rate'].astype(float) == float(line_dict['params']['learning_rate']))]['on_front'].values[0] == False:
+                continue
+            else: # If model on pareto front, check if already verified in linear scan results
+                key = f"{line_dict['dname']}_{line_dict['params']['n_estimators']}_{line_dict['params']['max_depth']}_{line_dict['params']['learning_rate']}_{line_dict['fold']}"
+                if key in results_linear_scan and results_linear_scan[key]['compression']['verification_results']['failed_oc'] == False \
+                    and 'time_taken_oc' in results_linear_scan[key]['compression']['verification_results']:
+                        print(json.dumps(results_linear_scan[key]))
+                else:
+                    results = {
+                        'date_time': util.nowstr(),
+                        'hostname': os.uname()[1],
+                        'dname': dname,
+                        'fold': fold,
+                        'seed': seed,
+                        'metric_name': line_dict['metric_name'],
+                        'mtrain': line_dict['mtrain'],
+                        'mvalid': line_dict['mvalid'],
+                        'mtest': line_dict['mtest'],
+                        'ntrees': line_dict['ntrees'],
+                        'nnodes': line_dict['nnodes'],
+                        'nleafs': line_dict['nleafs'],
+                        'params': line_dict['params'],
+                    }
+
+                    model = veritas.AddTree.from_json(line_dict['model_json'])
+
+                    results['compression'] = {
+                        'verification_results': run_verification_tasks(model, dtest.X, dtest.y, timeout=timeout, n=500),
+                        'nleafs': model.num_leafs(),
+                        'nnodes': model.num_nodes(),
+                        'observed_oc_space_training': calculate_observable_oc_space(x=dtrain.X.to_numpy(), at=model),
+                        'observed_oc_space_test': calculate_observable_oc_space(x=dtest.X.to_numpy(), at=model),
+                        'oc_score_training': calculate_oc_score(x=dtrain.X.to_numpy(), y=dtrain.y.to_numpy(), at=model),
+                        'oc_score_test': calculate_oc_score(x=dtest.X.to_numpy(), y=dtest.y.to_numpy(), at=model),
+                        'oc_space_bound': bound_oc_space(model),
+                        'mtest': dtest.metric(model),
+                        'mvalid': dvalid.metric(model),
+                        'mtrain': dtrain.metric(model)
+                    }
+
+                    print(json.dumps(results))
 
 
 
